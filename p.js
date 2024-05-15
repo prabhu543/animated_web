cae68a46f574e4d3731d6e08f16edbfed9d29655
Prabhu <prabhuprathamesh444@gmail.com>
2024-05-15 16:46:26 +0530

    animated website

---
+++ p.js
@@ -0,0 +1,23 @@
+
+gsap.to(".imgcontaiber",{
+    width:"100%",
+    ease:Expo.easeInOut,
+    stagger:2
+})
+gsap.to(".imgcontaiber",{
+    opacity:0,
+    ease:Expo.easeInOut,
+    stagger:2,
+    delay:2
+})
+gsap.to(".text h1",{
+    top:0,
+    ease:Expo.easeInOut,
+    stagger:2
+})
+gsap.to(".text h1",{
+    top:"-100%",
+    ease:Expo.easeInOut,
+    stagger:2,
+    delay:2
+})
